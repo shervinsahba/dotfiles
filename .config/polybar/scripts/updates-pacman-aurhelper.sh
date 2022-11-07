bffecd5b9c6fa6a0aad9d80db53ec1dc0c42bfba
Shervin Sahba <shervin@tuta.io>
2022-11-07 14:52:09 -0800

    overhauled polybar mine profile

--- .config/polybar/scripts/updates-pacman-aurhelper.sh
+++ .config/polybar/scripts/updates-pacman-aurhelper.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-COLOR_OS="#f087bd"
-SYMBOL_OS="" #""
+COLOR="#f087bd"
 
 if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
@@ -11,5 +10,9 @@ if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
 fi
 
 if [[ "$updates_arch" -gt 0 || "$updates_aur" -gt 0  ]]; then
-    echo "%{F$COLOR_OS}$SYMBOL_OS $updates_arch+$updates_aur%{F-}"
+    if [[ "$updates_arch" -gt 200 ]]; then
+        echo "%{F$COLOR}$updates_arch%{F-}+$updates_aur"
+    else
+        echo "$updates_arch+$updates_aur"
+    fi
 fi
